ae3d00d99246c5da202331673d768da0a07d752a
baorie <rmq6@duke.edu>
2018-08-15 16:57:58 -0400

    cleaned up spaghetti code

--- script.py
+++ script.py
@@ -1,39 +1,75 @@
 from __future__ import print_function
 import os, sys
 from PIL import Image
-from numpy import array
+import numpy as np
 
-im = Image.open("meme.jpg")
-maxsize = (320, 240)
-size = list(im.size)
-print("Successfully loaded image!")
-print("Image size: " + str(size[0]) + " x " + str(size[1]))
-im = im.resize([160,120], Image.ANTIALIAS)
-arr = array(im)
+ASCII_VALS = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+MAX_VAL = 255
 
-light_arr = [[ 0 for i in range(len(arr[0]))] for j in range(len(arr))]
+def load(filename):
+    return Image.open(filename)
 
-for x in range(len(arr)):
-    for y in range(len(arr[x])):
-        pixel = arr[x][y]
-        pixel_avg = (int(pixel[0]) + int(pixel[1]) + int(pixel[2]) ) / 3 
-        light_arr[x][y] = int(pixel_avg)
+def get_size(inp):
+    if isinstance(inp, (list, tuple, np.ndarray)):
+        dim = ( len(inp[0]), len(inp))
+    else:
+        dim = list(inp.size)
+    return (dim[0], dim[1])
 
-ascii_arr = [[ None for i in range(len(arr[0]))] for j in range(len(arr))]
-ascii_vals = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
-print(len(ascii_vals)) # 65 characters
+def resize(image, ratio):
+    r, c = get_size(image)
+    image = image.resize([int(ratio*r), int(ratio*c)], Image.ANTIALIAS)
+    return image
 
-# brightness values are between 0 and 255 (256 total values)
-# there are only 65 characters
+def image_to_array(image):
+    return np.array(image)
 
-interval_range = int(255/len(ascii_vals))
-print(interval_range)
+def generate_brightness_array(arr, bright_type):
+    dim = get_size(arr)
+    b_arr = [[ 0 for i in range(dim[0])] for j in range(dim[1])] #brightness_array
+    for x in range(dim[1]):
+        for y in range(dim[0]):
+            pixel = arr[x][y] #rgb tuple
+            r, g, b = int(pixel[0]), int(pixel[1]), int(pixel[2])
+            avg = (r + g + b) / 3
+            lightness = (max(r,g,b) + min(r,g,b)) / 2
+            luminosity = int(0.21*r + 0.72*g + 0.07*b)
+            if (bright_type == 1):
+                b_arr[x][y] = avg
+            elif (bright_type == 2):
+                b_arr[x][y] = lightness
+            elif (bright_type == 3):
+                b_arr[x][y] = luminosity
+    return b_arr
 
-for x in range(len(light_arr)):
-    for y in range(len(light_arr[x])):
-        brightness = int(light_arr[x][y]) #makes sure this is an integer between 0 and 255
-        ascii_arr[x][y] = ascii_vals[int(brightness/255*len(ascii_vals)) - 1]
+def generate_char_array(arr):
+    dim = get_size(arr)
+    ascii_arr = [[ None for i in range(dim[0])] for j in range(dim[1])]
+    interval = int(MAX_VAL/len(ASCII_VALS))
+    for x in range(dim[1]):
+        for y in range(dim[0]):
+            bright_val = int(arr[x][y])
+            index = int(bright_val/MAX_VAL*len(ASCII_VALS)) - 1
+            ascii_arr[x][y] = ASCII_VALS[index]
+    return ascii_arr
 
-for row in ascii_arr:
-    row_out = [ i + i + i for i in row]
-    print("".join(row_out))
+def print_ascii_image(arr):
+    for r in arr:
+        line = [ i + i + i for i in r]
+        print("".join(line))
+
+if __name__ == "__main__":
+    # import image
+    filename = input("Which file would you like to convert to ASCII?: ")
+    im = load(filename)
+    dim = get_size(im)
+    print("Current image size: " + str(dim[0]) + " x " + str(dim[1]))
+    ratio = int(input("Resize your image by percentage! Please enter a number between 0 and 100: "))/100
+    im = resize(im, ratio)
+    resize_dim = get_size(im)
+    print("New image size: " + str(resize_dim[0]) + " x " + str(resize_dim[1]))
+    a_arr = image_to_array(im)
+    brightness_type = int(input("Do you want to map brightness using average (1), lightness (2), or luminosity (3)?: "))
+    b_arr = generate_brightness_array(a_arr, brightness_type)
+    c_arr = generate_char_array(b_arr)
+    print_ascii_image(c_arr)
